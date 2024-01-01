901470d6aecdf861ebebdd77e9e4fe2b2939d2dd
Istvan Katsanyi <katsanyi@gmail.com>
2024-01-01 18:17:07 +0100

    16th

--- aoc16.py
+++ aoc16.py
@@ -1,34 +1,58 @@
-#import re
-import re
-import sys
+from dataclasses import dataclass
+
+@dataclass(unsafe_hash=True)
+class Position:
+    row: int
+    col: int
+    directions = {
+        'u': (-1, 0),
+        'd': (1, 0),
+        'l': (0, -1),
+        'r': (0, 1)
+    }
+    def step(self, direction):
+        self.row += self.directions[direction][0]
+        self.col += self.directions[direction][1]
 
 # upper left coord is (0,0)
 # goes by (row, col)
 
-# input: list of tuples (name, focal length), fl=0 means it's a -
-# data structure: array of list of tuples: (name, focal length)
 
-#dict: (direction arrived, place -> direction left
+#dict: (direction arrived, place) -> direction left
 stepd = {
-    ('r', '.'): 'r',
-    ('l', '.'): 'l',
-    ('u', '.'): 'u',
-    ('d', '.'): 'd',
-    ('r', '\\'): 'd',
-    ('l', '\\'): 'u',
-    ('u', '\\'): 'l',
-    ('d', '\\'): 'r',
-    ('r', '/'): 'u',
-    ('l', '/'): 'd',
-    ('u', '/'): 'r',
-    ('d', '/'): 'l'
+    ('r', '|'):  ['u','d'],
+    ('l', '|'):  ['u','d'],
+    ('u', '|'):  ['u'],
+    ('d', '|'):  ['d'],
+    ('r', '-'):  ['r'],
+    ('l', '-'):  ['l'],
+    ('u', '-'):  ['l', 'r'],
+    ('d', '-'):  ['l', 'r'],
+    ('r', '.'):  ['r'],
+    ('l', '.'):  ['l'],
+    ('u', '.'):  ['u'],
+    ('d', '.'):  ['d'],
+    ('r', '\\'): ['d'],
+    ('l', '\\'): ['u'],
+    ('u', '\\'): ['l'],
+    ('d', '\\'): ['r'],
+    ('r', '/'):  ['u'],
+    ('l', '/'):  ['d'],
+    ('u', '/'):  ['r'],
+    ('d', '/'):  ['l']
 }
 
-def move(d, p, row, col):
+def move(d, p):
+    '''
+
+    :param d: arriving direction
+    :param p: symbol at positon
+    :return: list of outgoing directions (1 or 2 directions)
+    '''
     ret = stepd[(d,p)]
     return ret
 
-def print_world(world):
+def xprint_world(world):
     for i in world:
         print(*i)
 
@@ -46,35 +70,62 @@ def read(fname):
     fc = readfile(fname)
     return fc
 
-
-def process(input):
-    boxes = [[] for i in range(256)]
-    for i in input:
-        h = hash(i[0])
-        if i[1] > 0:
-            found = False
-            for j in range(len(boxes[h])):
-                if boxes[h][j][0] == i[0]:
-                    boxes[h][j] = (i[0], i[1])
-                    found = True
-            if not found:
-                boxes[h].append(i)
-        else:
-            for j in boxes[h]:
-                if j[0] == i[0]:
-                    boxes[h].remove(j)
-    return boxes
-
-# returns the fp of a single box
-# input is the box itself and its seq number
-def focus_power(b, bno):
-    s = 0
-    for i in range(len(b)):
-        s += (bno + 1) * (i + 1) * b[i][1]
+def process0(slist: list[str], start):
+    energized = set()
+    visited = set()
+    current = [start]
+#    zz = Position(0,0)
+#    current = [(zz, 'r')]
+    while len(current) > 0:
+        a = current.pop()
+        cp = a[0]
+        cd = a[1]
+        energized.add(cp)
+        visited.add(a)      # Tuning tip it's enough to differentiate if the position was visited horizontally or vertically
+#        print()
+#        print('cp:', cp)
+#        print('current:', current)
+#        print('energized:', energized)
+        p = slist[cp.row][cp.col]
+        directions = move(cd,p)
+        for i in directions:
+            newpos = Position(cp.row, cp.col)
+            newpos.step(i)
+            if (newpos, i) in visited:
+                pass
+            elif newpos.row < 0 or newpos.row >= len(slist) or newpos.col < 0 or newpos.col >= len(slist[0]):
+                pass
+            else:
+                current.append((newpos, i))
+    s = len(energized)
     return s
 
-def score(boxes):
+def process(slist: list[str]):
+    start = (Position(0, 0), 'r')
+#    start = (Position(0, 3), 'd')
+    return process0(slist, start)
+
+def process2(slist: list[str]):
+    maxe = 0
+    nrow = len(slist)
+    ncol = len(slist[0])
+    for col in range(ncol):
+        start = (Position(0, col), 'd')
+        r = process0(slist, start)
+        if r > maxe: maxe = r
+        start = (Position(nrow - 1, col), 'u')
+        r = process0(slist, start)
+        if r > maxe: maxe = r
+    for row in range(nrow):
+        start = (Position(row, 0), 'r')
+        r = process0(slist, start)
+        if r > maxe: maxe = r
+        start = (Position(row, ncol - 1), 'l')
+        r = process0(slist, start)
+        if r > maxe: maxe = r
+    return maxe
+
+
+def score():
     s = 0
-    for i in range(len(boxes)):
-        s += focus_power(boxes[i],i)
     return s
--- test_aoc16.py
+++ test_aoc16.py
@@ -1,29 +1,63 @@
 import pytest
 import aoc16
 
+@pytest.mark.parametrize('start, dir, expected', [
+    (aoc16.Position(10, 20), 'l', aoc16.Position(10, 19)),
+    (aoc16.Position(10, 20), 'r', aoc16.Position(10, 21)),
+    (aoc16.Position(10, 20), 'u', aoc16.Position( 9, 20)),
+    (aoc16.Position(10, 20), 'd', aoc16.Position(11, 20))
+])
+def test_step(start, dir, expected):
+    print('expected:', expected)
+    start.step(dir)
+    print(start)
+    assert start == expected
+
 @pytest.mark.parametrize('d, p, expected', [
-    ('r', '.', 'r'),
-    ('l', '.', 'l'),
-    ('u', '.', 'u'),
-    ('d', '.', 'd'),
-    ('r', '\\', 'd'),
-    ('l', '\\', 'u'),
-    ('u', '\\', 'l'),
-    ('d', '\\', 'r'),
-    ('r', '/', 'u'),
-    ('l', '/', 'd'),
-    ('u', '/', 'r'),
-    ('d', '/', 'l')
+    ('r', '|', ['u', 'd']),
+    ('l', '|', ['u', 'd']),
+    ('u', '|', ['u']),
+    ('d', '|', ['d']),
+    ('r', '-', ['r']),
+    ('l', '-', ['l']),
+    ('u', '-', ['l', 'r']),
+    ('d', '-', ['l', 'r']),
+    ('r', '.', ['r']),
+    ('l', '.', ['l']),
+    ('u', '.', ['u']),
+    ('d', '.', ['d']),
+    ('r', '\\', ['d']),
+    ('l', '\\', ['u']),
+    ('u', '\\', ['l']),
+    ('d', '\\', ['r']),
+    ('r', '/', ['u']),
+    ('l', '/', ['d']),
+    ('u', '/', ['r']),
+    ('d', '/', ['l'])
 ])
 def test_move(d,p,expected):
-    assert aoc16.move(d,p,0,0) == expected
+    assert aoc16.move(d,p) == expected
 
-def xtest_score1():
-    assert aoc15.score1('HASH') == 52
+@pytest.fixture
+def slist_():
+    return aoc16.read(r'C:\py\github\aoc2023\16_.dat')
 
-def xtest_score():
-    input = aoc15.read(r'C:\py\github\aoc2023\16_.dat')
-    assert aoc15.score(input) == 1320
+@pytest.fixture
+def slist():
+    return aoc16.read(r'C:\py\github\aoc2023\16.dat')
+
+def test_process(slist_):
+    assert aoc16.process(slist_) == 46
+
+def test_process2(slist_):
+    assert aoc16.process2(slist_) == 51
+
+def test_process2(slist):
+    assert aoc16.process2(slist) == 7313
+
+def test_process(slist):
+    print()
+    assert aoc16.process(slist) == 7046
 
 
 
